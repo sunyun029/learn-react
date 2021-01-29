2e4f0bf2b325d91559718d353c4a41a6a7641608
sun.yun <sun.yun@hendonsoft.com>
2021-01-29 16:56:40 +0800

    practice JSX

--- src/index.js
+++ src/index.js
@@ -105,12 +105,110 @@ class Game extends React.Component {
   }
 }
 
-// ========================================
+const user = {
+  firstName:'Happer',
+  lastName:'Peraz',
+  avatarUrl:"http://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png"
+};
+// const element = (
+//   getGreeting(user)
+// );
+function formatName(user){
+  return user.firstName+' '+user.lastName;
+}
+function getGreeting(user){
+  if(user){
+    return<h1> hello,{formatName(user)}!</h1>
+  }
+  return <h1>hello,stranger.</h1>
+}
+// const element = (
+//   <div>
+//     <img src={user.avatarUrl} />
+//     <h1>Hello!</h1>
+//     <h2>Good to see you here.</h2>
+// </div>
+// );
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById('root'));
+// }
 
+// setInterval(tick, 1000);
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+function Comment(props){
+  return(
+    <div className="Comment">
+      <div className="UserInfo">
+      <img className="Avatar"
+        src={props.author.avatarUrl}
+        alt={props.author.name}
+      />
+      <div className="UserInfo-name">
+        {props.author.name}
+      </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
 ReactDOM.render(
-  <Game />,
+  <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+  />,
   document.getElementById('root')
 );
+// const element = <Welcome name="Sara" />;
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
+// ========================================
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
+// ReactDOM.render(
+//   element,
+//   <Game />,
+//   document.getElementById('root')
+// );
 
 
 // If you want to start measuring performance in your app, pass a function
@@ -135,3 +233,4 @@ function calculateWinner(squares) {
   }
   return null;
 }
+
