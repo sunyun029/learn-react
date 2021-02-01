2123ddd821ad98226f92e25e039842c9793fe256
sun.yun <sun.yun@hendonsoft.com>
2021-02-01 17:30:01 +0800

    practice组件

--- src/index.js
+++ src/index.js
@@ -157,18 +157,30 @@ function App() {
 function formatDate(date) {
   return date.toLocaleDateString();
 }
+//组件构建过程↓
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  );
+}
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+
 function Comment(props){
   return(
     <div className="Comment">
-      <div className="UserInfo">
-      <img className="Avatar"
-        src={props.author.avatarUrl}
-        alt={props.author.name}
-      />
-      <div className="UserInfo-name">
-        {props.author.name}
-      </div>
-      </div>
+      <UserInfo user= {props.author}/>
       <div className="Comment-text">
         {props.text}
       </div>
